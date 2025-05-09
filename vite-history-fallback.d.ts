8d222fb821686eb7c8b177bc62f7aa6cdedbc6ec
bdleasure <bdleasure@gmail.com>
2025-05-09 18:55:56 -0500

    updated cloudflare worker name to impossible-agent - 2025-05-09 18:55:36

--- vite-history-fallback.d.ts
+++
@@ -1,7 +0,0 @@
-// vite-history-fallback.d.ts
-declare module './vite-history-fallback' {
-  export default function historyFallback(): {
-    name: string;
-    configureServer(server: any): () => void;
-  };
-}
--- vite-spa-plugin.d.ts
+++
@@ -1,7 +0,0 @@
-// vite-spa-plugin.d.ts
-declare module './vite-spa-plugin' {
-  export default function viteSpaPlugin(): {
-    name: string;
-    configureServer(server: any): () => void;
-  };
-}
